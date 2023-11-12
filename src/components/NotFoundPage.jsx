d219b4743e23d38e7d3ca6aa29bc508804dc656c
Umamaheswari Thiyagarajan <umagithub23@users.noreply.github.com>
2023-11-12 08:01:54 +0100

    Styling updates

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -1,16 +1,14 @@
 import { Link } from "react-router-dom";
 
-import backButton from "../assets/back-arrow-button.svg";
+import "./NotFoundPage.css";
 
 export const NotFoundPage = () => {
   return (
-    <div>
+    <div className="not-found-page">
       <h1>
         Oh Uh, it seems that the page you are trying to access does not exist.
       </h1>
-      <Link to="/" className="backLink">
-        <img src={backButton} color="white" /> Movies
-      </Link>
+      <Link to="/">Go Home</Link>
     </div>
   );
 };
